ce7065bcff5b93cce9631e202f93723a0841ef1c
sayansen1711 <sayansen1711@gmail.com>
2025-12-29 00:07:03 +0530

    Todo DTO fixed

--- TodosApplication/src/main/java/com/project/TodosApplication/dto/TodoDTO.java
+++ TodosApplication/src/main/java/com/project/TodosApplication/dto/TodoDTO.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 
 public class TodoDTO {
 
+    private String username; //read-only
     private int id;
     @NotNull
     @Size(min = 10, message = "Enter at least 10 characters")
@@ -37,6 +38,14 @@ public class TodoDTO {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+    
     public String getDescription() {
         return description;
     }
